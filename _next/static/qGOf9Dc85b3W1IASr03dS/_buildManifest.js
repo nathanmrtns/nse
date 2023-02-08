15b2765c1d64f35fb8fa83a0a37b209aaf62b6ab
nathanmrtns <nathanmrtns@gmail.com>
2023-02-08 00:05:54 +0000

    Deploying to gh-pages from @ nathanmrtns/nse@d8fe98f755ee65af4c756c99f2a7a2d9161fc7a2 🚀

---
+++ _next/static/qGOf9Dc85b3W1IASr03dS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/21f33a02d6d58575.css","static/chunks/pages/index-082765ab7076dee5.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
